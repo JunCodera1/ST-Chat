24e3f8fb00dd76fb52b22ca0f124e3919b58b22d
JunCodera1 <minhtien0601200@gmail.com>
2025-06-07 06:42:42 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module me.chatapp.stchat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens me.chatapp.stchat to javafx.fxml;
+    exports me.chatapp.stchat;
+}
