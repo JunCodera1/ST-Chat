7d26bffed94ce9cb5721a5a80796cc2841004544
JunCodera1 <minhtien0601200@gmail.com>
2025-07-14 15:08:30 +0700

    ➕ Add TriConsumer functional interface

--- stchat-server/src/main/java/com/stchat/server/web/mapper/CustomJacksonMapperImpl.java
+++
@@ -1,9 +0,0 @@
-package com.stchat.server.web.mapper;
-
-import io.javalin.json.JsonMapper;
-
-public interface CustomJacksonMapperImpl extends JsonMapper {
-    String toJsonString(Object obj);
-
-    <T> T fromJsonString(String json, Class<T> targetType);
-}
